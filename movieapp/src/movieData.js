a1eee38385d794d6ef0e4eb29b7031e63b795ceb
youssef <bufalgha@gmail.com>
2022-11-28 09:33:55 +0100

    first commit

---
+++ movieapp/src/movieData.js
@@ -0,0 +1,90 @@
+export const moviesData = [
+    {
+      id: 1,
+      image:
+        "https://i.inews.co.uk/content/uploads/2020/04/money-heist-first-impression-1200-640x360.jpg",
+      rating: 4,
+      name: "casa del papel",
+      date: "1976",
+      type: "Crime | Drama",
+      seasons: "2 seasons",
+      description:
+        "A mentally unstable driver in Ned sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute."
+    },
+  {
+      id: 2,
+      image:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/papillon-2017-charlie-hunnam-y-rami-malek-1548866422.jpg",
+      rating: 1,
+      name: "Papillon",
+      date: "2006",
+      type: "Comedy | Drama",
+      seasons: "4 seasons",
+      description:
+        "A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus."
+    },
+  {
+      id: 3,
+      image:
+        "https://thebishopstower.com/wp-content/uploads/2021/02/queens-gambit.jpg",
+      rating: 3,
+      name: "Queen's Gambit",
+      date: "2006",
+      type: "Comedy | Drama",
+      seasons: "4 seasons",
+      description:
+        "A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus."
+    },
+  
+  {
+      id: 4,
+      image:
+        "https://s4844.pcdn.co/wp-content/uploads/2014/11/interstellar-photos-pictures-stills.jpg",
+      rating: 4,
+      name: "Mad Max",
+      date: "1979",
+      type: "Action | Adventure",
+      seasons: "5 seasons",
+      description:
+        "In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang."
+    },
+    {
+      id: 5,
+      image:
+        "https://customonesixth.com/attachments/kakaotalk_20210601_001650757_01-jpg.434178/",
+      rating: 3,
+      name: "Little Miss Sunshine",
+      date: "2006",
+      type: "Comedy | Drama",
+      seasons: "4 seasons",
+      description:
+        "A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus."
+    },
+    {
+      id: 6,
+      image:
+        "https://i.inews.co.uk/content/uploads/2020/04/money-heist-first-impression-1200-640x360.jpg",
+      rating: 4,
+      name: "Mad Max",
+      date: "1979",
+      type: "Action | Adventure",
+      seasons: "5 seasons",
+      description:
+        "In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang."
+    },
+
+  {
+      id: 7,
+      image:
+        "https://cdn1.ntv.com.tr/gorsel/mWUBO2O0gESc9db9rQHfJw.jpg?width=1000&mode=crop&scale=both",
+      rating: 5,
+      name: "KILL BILL: VOL. 1",
+      date: "2003",
+      type: "Action",
+      seasons: "2 seasons",
+      description:
+        "The lead character, called 'The Bride,' was a member of the Deadly Viper Assassination Squad, led by her lover 'Bill.'The Bride' decided to escape her life as a killer.",
+      details: "https://www.imdb.com/title/tt0266697/plotsummary?ref_=tt_stry_pl"
+    }
+  ];
+  
